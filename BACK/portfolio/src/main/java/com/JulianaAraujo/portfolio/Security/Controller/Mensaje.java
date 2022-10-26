7338e4ed3562e5042366f4c8b4c2c990a97d6491
juliana3 <99768491+juliana3@users.noreply.github.com>
2022-10-26 02:41:22 -0300

    zcf

---
+++ BACK/portfolio/src/main/java/com/JulianaAraujo/portfolio/Security/Controller/Mensaje.java
@@ -0,0 +1,28 @@
+
+package com.JulianaAraujo.portfolio.Security.Controller;
+
+
+public class Mensaje {
+    private String mensaje;
+    
+    //CONSTRUCTORES
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    
+    //getters y setters
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+}
